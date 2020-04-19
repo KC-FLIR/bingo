52a1f80e31ba638061fa743007e8c746bf010f08
KevC <kc2u.iphone@gmail.com>
2020-04-19 10:12:25 +0100

    coloured balls, better offset calculations

--- bingoballer.py
+++ bingoballer.py
@@ -7,6 +7,17 @@ import numpy as np
 from random import seed
 from random import randint as randomint
 
+ball_colours = [(128, 0, 0),
+                (128, 128, 0),
+                (0, 128, 0),
+                (77, 128, 0),
+                (0, 128, 77),
+                (0, 34, 200),
+                (34, 128, 200),
+                (34, 200, 100),
+                (200, 128, 200),
+                (255, 28, 144)
+                ]
 
 def blank_balls(width, height, rgb_colour=(0, 0, 0)):
     """Create new image(numpy array) filled with certain color in RGB"""
@@ -52,11 +63,9 @@ def render_sballs(image=None, numbertext=None):
 
     size = 100
     thickness = int(size/10)
-    radius = int ((size-(2*thickness)) / 2)
     radius = int ((size-thickness) / 2)
     centre = int (size / 2 )
-    fudge = int(centre/2)
-
+    
     if image is None:
         image = blank_balls(size, size)
 
@@ -65,7 +74,7 @@ def render_sballs(image=None, numbertext=None):
 
     if numbertext is not None:
         font = cv2.FONT_HERSHEY_COMPLEX_SMALL 
-        org = ((int(size/3.3)),(int(size/1.7))) 
+        org = ((int(size/3.5)),(int(size/1.7))) 
         fontScale = 2
         color = (0,0,0)
         textthickness = int (thickness/2)
@@ -73,6 +82,43 @@ def render_sballs(image=None, numbertext=None):
 
 
     return image
+
+def render_scballs(image=None, number=None):
+
+    size = 100
+    thickness = int(size/10)
+    radius = int ((size-thickness) / 2)
+    centre = int (size / 2 )
+    ThisBallColour = ball_colours[0]
+
+    if number is not None:
+        colour_index = int(number/10)
+        #print("Balls please :{} colour index {}".format(number,colour_index))
+        ThisBallColour = ball_colours[colour_index]
+    
+    if image is None:
+        image = blank_balls(size, size)
+
+    cv2.circle(image, (centre, centre), radius, (255, 255, 255), -1)
+    #cv2.circle(image, (centre, centre), radius, (139, 0, 139), thickness)
+    cv2.circle(image, (centre, centre), radius, ThisBallColour, thickness)
+
+    if number is not None:
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL 
+        y = int(size/1.63)
+        if number<10:
+            x = int(size/2.8)
+        else:
+            x = int(size/3.8)
+        
+        org = (x,y) 
+        fontScale = 2
+        color = (0,0,0)
+        textthickness = int (thickness/2)
+        cv2.putText(image, "{}".format(number), org, font,      fontScale, color, textthickness, cv2.LINE_AA) 
+
+    return image
+
 
 
 numbers2do = 90
@@ -82,7 +128,8 @@ while numbers2do > 0:
     thisrando = randomint(1,90)
     if thisrando not in picked:
         picked.append(thisrando)
-        image = render_sballs(numbertext='{}'.format(thisrando))
+        #print("Rando {}".format(thisrando))
+        image = render_scballs(number=thisrando)
         numbers2do -= 1
         cv2.imshow('Test image', image)
         cv2.waitKey(500)
