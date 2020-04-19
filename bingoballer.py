d2be143900a1add81741481dea9ba003e0ee4551
KevC <kc2u.iphone@gmail.com>
2020-04-19 15:26:32 +0100

    apply correction from card generator

--- bingoballer.py
+++ bingoballer.py
@@ -133,14 +133,14 @@ class ballBox:
     def __init__(self, name=None):
         self.name = name
     
-    def create(self):
-        self.canvas_width = (self.ballsize*(self.columns+1))+(self.ballpadding*(self.columns+1))+(self.border*2)
-        self.canvas_height = (self.ballsize*(self.rows+1))+(self.ballpadding*(self.rows + 1))+(self.border*2)
-        #boxWidth = (ballSize*11)+(padding*11)+(border*2)
-        self.image = blank_balls(self.canvas_width, self.canvas_height)
-        print("Big Box is: width {}  height {}".format(self.canvas_width, self.canvas_height))
-        cv2.rectangle(self.image, (0, 0), (self.canvas_width, self.canvas_height), (200, 200, 200), self.border)
 
+    def create(self):
+        total_image_dimension = self.ballsize+(self.ballpadding*2) 
+        self.canvas_width = (total_image_dimension*self.columns)+(self.border*2)
+        self.canvas_height = (total_image_dimension*self.rows)+(self.border*2)
+        #print("Big Box is: (rows {}, cols {}) width {}  height {}".format(self.rows,self.columns,self.canvas_width, self.canvas_height))
+        self.image = blank_balls(self.canvas_width, self.canvas_height)
+        cv2.rectangle(self.image, (0, 0), (self.canvas_width, self.canvas_height), (200, 200, 200), self.border)
         return self.image
 
     def placeBall(self,ballImage=None,index=0):
@@ -155,7 +155,7 @@ class ballBox:
         ball_x = box_x + padding
         ball_y = box_y + padding
 
-        print("Ball #{} Box({},{})  Ball({},{})".format(index, box_x, box_y, ball_x, ball_y))
+        #print("Ball #{} Box({},{})  Ball({},{})".format(index, box_x, box_y, ball_x, ball_y))
 
         cv2.rectangle(self.image, (box_x, box_y), (box_x + box_width, box_y + box_width), (255, 10, 10), 1)
 
@@ -201,7 +201,7 @@ while numbersDone < numbers2do:
         box.show()
 
         numbersDone += 1
-        cv2.waitKey(5000)
+        cv2.waitKey(0)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
