9403592203f8a3a1b1c9652d2ff4057618102d20
KevC <kc2u.iphone@gmail.com>
2021-05-25 20:17:06 +0100

    aDD SCRABBLE-DISTRIBUTION LETTER DICT

--- countdown.py
+++ countdown.py
@@ -136,6 +136,7 @@ class lettershelf(object):
 vowels = "AEIOU"
 consonants = "BCDFGHJKLMNPQRSTVWXYZ"
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SCRABBLE = dict(A=9,B=2,C=2,D=4,E=12,F=2,G=3,H=2,I=9,J=1,K=1,L=4,M=2,N=6,O=8,P=2,Q=1,R=6,S=4,T=6,U=4,V=2,W=2,X=1,Y=2,Z=1)
 
 
 # Must have 3 vowels
